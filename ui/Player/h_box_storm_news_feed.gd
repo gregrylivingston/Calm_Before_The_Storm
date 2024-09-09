37f2b9121c575c0bf2337cbb848f34f84e72dae3
gregrylivingston <livingsgb@gmail.com>
2024-09-08 22:56:30 -0400

    better weather alerts

--- ui/Player/h_box_storm_news_feed.gd
+++ ui/Player/h_box_storm_news_feed.gd
@@ -5,6 +5,7 @@ extends Label
 func _ready() -> void:
 	visible = false
 	Weather.new_weather_state.connect(_new_weather_state)
+	$HBox_StormNewsFeed/VSeparator.custom_minimum_size.x = get_viewport_rect().size.x
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,11 +14,11 @@ func _process(delta: float) -> void:
 
 
 func _new_weather_state(newState:WeatherState) -> void:
-	%Label_WeatherInfo.text  = "Expect " + newState.title + "."
+	%Label_WeatherInfo.text  = newState.title + "."
 	if Weather.weather_state_int > 4:
 		%Label_WeatherInfo.text += "  Flooding imminent.  Seek higher ground."
 	
-	position.x = 0#(get_viewport_rect().size.x / 2) - 100
+	position.x = 0 - size.x/2#(get_viewport_rect().size.x / 2) - 100
 	visible = true
 	await  get_tree().create_timer(.25).timeout
 	visible = false
