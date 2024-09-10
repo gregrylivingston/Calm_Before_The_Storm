418d7cdaa97728a8e1e5ec1ddf14ca1aca37a114
gregrylivingston <livingsgb@gmail.com>
2024-09-10 13:43:04 -0400

    upgrade items panel

---
+++ ui/Player/hbox_upgrade_item.gd
@@ -0,0 +1,19 @@
+extends HBoxContainer
+
+var title: String
+var cost: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Label_Name.text = title.replace("_"," ")
+	call_deferred("_update_values")
+
+
+func _update_values():
+	$Label2_Value.text = str(Player.data.upgrade[title] - 1)
+	cost = Player.data.upgrade[title] * Player.data.upgrade[title]
+	$Button_Upgrade.text = str(cost)
+	
+	if Player.data.stars < cost:
+		$Button_Upgrade.disabled = true
+	
--- ui/menu/upgrades_list.gd
+++ ui/menu/upgrades_list.gd
@@ -1,11 +1,14 @@
 extends VBoxContainer
 
+var item_upgrade_hbox = load("res://ui/Player/hbox_upgrade_item.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for upgrade in Player.data.upgrade:
-		var title = upgrade.replace("_"," ")
-		var value = Player.data.upgrade[upgrade]
+		var newBox = item_upgrade_hbox.instantiate()
+		newBox.title = upgrade
+		add_child(newBox)
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
